25380eacdb5c1c6037a1c57d35cfb605b33f655d
Kevin Rothenberger <kevin.rothenberger@gmail.com>
2014-12-19 15:27:51 -0500

    Added integration test framework

---
+++ src/integration-test/java/ITMTagsAPI.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by kevinrothenberger on 12/19/14.
+ */
+public class ITMTagsAPI {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
